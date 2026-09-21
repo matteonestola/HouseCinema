f23a634d105225b5a8c6cc9f3f79af50527c48c9
matteonestola <matty.nestola@gmail.com>
2026-09-22 01:13:04 +0200

    add configuration

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,11 @@
 // Sostituisci questi valori con quelli del TUO progetto Firebase.
 // Li trovi in: Console Firebase -> Impostazioni progetto -> Generali -> Le tue app -> App web -> SDK setup and configuration
 export const firebaseConfig = {
-  apiKey: "INCOLLA_QUI_API_KEY",
-  authDomain: "INCOLLA_QUI.firebaseapp.com",
-  projectId: "INCOLLA_QUI_PROJECT_ID",
-  storageBucket: "INCOLLA_QUI.appspot.com",
-  messagingSenderId: "INCOLLA_QUI",
-  appId: "INCOLLA_QUI"
+  apiKey: "AIzaSyAISJ2ZR_FXWvHXU4QkD5_Nzbvd920LzTQ",
+  authDomain: "housecinema-9eae5.firebaseapp.com",
+  projectId: "housecinema-9eae5",
+  storageBucket: "housecinema-9eae5.firebasestorage.app",
+  messagingSenderId: "446744893323",
+  appId: "1:446744893323:web:5ce06bc2b617c953db818e",
+  measurementId: "G-VCGDL623PP"
 };
